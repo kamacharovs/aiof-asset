1bde5be7c2dcd437b040b38e3e7595bceffcff9f
Georgi Kamacharov <gkamacharov@gmail.com>
2021-04-12 13:00:56 -0400

    update to remove data annotations

--- aiof.asset.data/AssetType.cs
+++ aiof.asset.data/AssetType.cs
@@ -2,17 +2,12 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Text.Json.Serialization;
-using System.ComponentModel.DataAnnotations;
 
 namespace aiof.asset.data
 {
     public class AssetType : IAssetType
     {
-        [Required]
-        [MaxLength(100)]
         public string Name { get; set; }
-
-        [Required]
         public Guid PublicKey { get; set; } = Guid.NewGuid();
     }
 }
